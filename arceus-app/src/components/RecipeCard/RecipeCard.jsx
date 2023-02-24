c1fe67807e2d1dd82b6352d38cb9b42abc09a67c
ShaoWei <laushaowei@gmail.com>
2023-02-25 03:50:05 +0800

    added HistoryCard, RecipeCard

--- arceus-app/src/components/RecipeCard/RecipeCard.jsx
+++ arceus-app/src/components/RecipeCard/RecipeCard.jsx
@@ -1,8 +1,27 @@
 import "./RecipeCard.css";
-
-// TODO
-const RecipeCard = () => {
-  return;
+const RecipeCard = (props) => {
+  const result = [];
+  for (let i = 0; i < props.rating; i++) {
+    result.push(<span>⭐</span>);
+  }
+  return (
+    <div className="recipe-card-container">
+      <div className="recipe-nutrients">
+        <h1>{props.name}</h1>
+        <p>{result}</p>
+        <p>
+          Carbohydrates: <span>{props.carbs}g</span>
+        </p>
+        <p>
+          Protein: <span>{props.protein}g</span>
+        </p>
+        <p>
+          Fats: <span>{props.fats}g</span>
+        </p>
+      </div>
+      <img className="recipe-image" src={props.img}></img>
+    </div>
+  );
 };
 
 export default RecipeCard;
