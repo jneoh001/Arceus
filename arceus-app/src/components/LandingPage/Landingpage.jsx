fd552e1860cded51a8e44bb7bcc6e221318fb59b
ShaoWei <laushaowei@gmail.com>
2023-03-15 23:20:13 +0800

    refined

--- arceus-app/src/components/LandingPage/Landingpage.jsx
+++ arceus-app/src/components/LandingPage/Landingpage.jsx
@@ -1,30 +1,28 @@
-import React from 'react';
-import Navbar from '../Navbar/Navbar';
-import RecipeCard from '../RecipeCard/RecipeCard';
-import RecommendedRecipes from '../RecipeCard/RecommendedRecipes';
-import './LandingPage.css';
-import '../Navbar/Navbar.css';
-import '../RecipeCard/RecipeCard.css';
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import RecipeCard from "../RecipeCard/RecipeCard";
+import RecommendedRecipes from "../RecipeCard/RecommendedRecipes";
+import "./LandingPage.css";
 
-const LandingPage = () =>{
-    return (
-        <div className = 'LandingPage'>
-            <Navbar/>
-            <h1 className= 'landingpageheader'>Recommended Recipes</h1>
-            <div className = 'recipecards'>
-                <RecipeCard/>
-                <RecipeCard/>
-                <RecipeCard/>
-                <RecipeCard/>
-                <RecipeCard/>
-                {/*<RecommendedRecipes/>
-                <RecommendedRecipes/>
-                <RecommendedRecipes/>
-                <RecommendedRecipes/>
-    <RecommendedRecipes/>*/}
-            </div>
-        </div>
-    );
-}
+const LandingPage = () => {
+  return (
+    <div className="LandingPage">
+      <Navbar />
+      <h1 className="landingpageheader">Recommended Recipes</h1>
+      <div className="recipecards">
+        {/* <RecipeCard />
+        <RecipeCard />
+        <RecipeCard />
+        <RecipeCard />
+        <RecipeCard /> */}
+        <RecommendedRecipes />
+        <RecommendedRecipes />
+        <RecommendedRecipes />
+        <RecommendedRecipes />
+        <RecommendedRecipes />
+      </div>
+    </div>
+  );
+};
 
 export default LandingPage;
