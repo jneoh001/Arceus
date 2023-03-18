ddf3c3170f4bf410800866cc05813f1a72ed01b0
Javier Neoh <jneoh001@e.ntu.edu.sg>
2023-03-18 18:06:52 +0800

    Updated with backend logic

--- arceus-app/src/components/RecipePage/RecipePage.jsx
+++ arceus-app/src/components/RecipePage/RecipePage.jsx
@@ -2,12 +2,10 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './RecipePage.css';
 import RecipeFavourite from './RecipeFavourite';
+import { db } from "../../firebaseConfig";
+import { useAuth } from "../../store/auth-context";
+import { ref, child, get, update, onValue } from "firebase/database";
 
-
-const result = [];
-for (let i = 0; i < 5; i++) {
-    result.push(<span>⭐</span>);
-}
 
 export default function RecipePage() {
 
@@ -15,9 +13,135 @@ export default function RecipePage() {
     const [recipeData, setRecipeData] = useState({});
     const [ingredientWidget, setIngredientWidget] = useState();
 
+    const [profile, setProfile] = useState({
+        email: "",
+        height: 0,
+        weight: 0,
+        carbGoal: 0,
+        proteinGoal: 0,
+        fatGoal: 0,
+        calorieGoal: 0,
+      });
+    
+      const [intake, setIntake] = useState({
+        carb: 0,
+        protein: 0,
+        fat: 0,
+        calorie: 0,
+      });
+    
+      const { currentUser } = useAuth();
+      const dbRef = ref(db);
+    
+      // Get today's date in the format of DD/MM/YYYY
+      const getDate = () => {
+        let today = new Date();
+        let year = today.getFullYear().toString().slice(-2);
+        let month = (today.getMonth() + 1).toString().padStart(2, "0");
+        let day = today.getDate().toString().padStart(2, "0");
+        let formattedDateID = `${day}-${month}-${year}`;
+        let formattedDateDisplay = `${day}/${month}/${year}`;
+        return [formattedDateID, formattedDateDisplay];
+      };
+    
+      //Update database once the intake changes
+      useEffect(() => {
+        const [todayID, todayDisplay] = getDate();
+        if (currentUser) {
+          const updates = {};
+          updates["/users-profile/" + currentUser.uid + "/history/" + todayID] = {
+            date: todayDisplay,
+            carbGoal: profile.carbGoal,
+            proteinGoal: profile.proteinGoal,
+            fatGoal: profile.fatGoal,
+            calorieGoal: profile.calorieGoal,
+            carbIntake: intake.carb,
+            proteinIntake: intake.protein,
+            fatIntake: intake.fat,
+            calorieIntake: intake.calorie,
+          };
+          update(ref(db), updates);
+        }
+      }, [intake, profile]);
+    
+      // Get the user's details when the user first log in
+      useEffect(() => {
+        if (currentUser) {
+          get(child(dbRef, "users-profile/" + currentUser.uid + "/details"))
+            .then((snapshot) => {
+              if (snapshot.exists()) {
+                setProfile(snapshot.val());
+              } else {
+                console.log("No data available");
+              }
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+    
+          const [todayID, todayDisplay] = getDate();
+          get(
+            child(dbRef, "users-profile/" + currentUser.uid + "/history/" + todayID)
+          )
+            .then((snapshot) => {
+              if (snapshot.exists()) {
+                setIntake({
+                  carb: snapshot.val().carbIntake,
+                  protein: snapshot.val().proteinIntake,
+                  fat: snapshot.val().fatIntake,
+                  calorie: snapshot.val().calorieIntake,
+                });
+                // console.log(snapshot.val());
+                // console.log("History retrieved!");
+              }
+            })
+            .catch((error) => {
+              console.log(error.code);
+            });
+    
+          onValue(
+            child(ref(db), "users-profile/" + currentUser.uid + "/details"),
+            (snapshot) => {
+              setProfile((pre) => {
+                return {
+                  ...profile,
+                  carbGoal: snapshot.val().carbGoal,
+                  proteinGoal: snapshot.val().proteinGoal,
+                  fatGoal: snapshot.val().fatGoal,
+                  calorieGoal: snapshot.val().calorieGoal,
+                };
+              });
+            }
+          );
+        }
+      }, [currentUser]);
+    
+      const addHandler = () => {
+        setIntake((pre) => {
+          return {
+            carb: pre.carb + recipeData.recipeCarbs,
+            protein: pre.protein + recipeData.recipeProtein,
+            fat: pre.fat + recipeData.recipeFat,
+            calorie: pre.calorie + recipeData.recipeCalorie,
+          };
+        });
+      };
+    
+      const subtractHandler = () => {
+        setIntake((pre) => {
+          return {
+            carb: pre.carb - 10,
+            protein: pre.protein - 10,
+            fat: pre.fat - 10,
+            calorie: pre.calorie - 10,
+          };
+        });
+      };
+    
+
     useEffect( ()=> {
         axios
-            .get("https://api.spoonacular.com/recipes/6/information?apiKey=" + apiKey)
+            .get("https://api.spoonacular.com/recipes/10123/information?apiKey=" + apiKey)
             // .then(response => {
             //     //console.log(response)
             //     setRecipeData({img: response.data.img})
@@ -29,6 +153,28 @@ export default function RecipePage() {
                     img: response.data.image,
                     instructions: response.data.instructions,
                     // title: response.data.title,
+                }
+                )
+            })
+
+            .catch( (error)=>console.log(error))
+    }, [])
+
+    useEffect( ()=> {
+        axios
+            .get("https://api.spoonacular.com/food/menuitems/10123?apiKey=" + apiKey)
+            // .then(response => {
+            //     //console.log(response)
+            //     setRecipeData({img: response.data.img})
+            //     console.log(recipeData)
+                
+            // })
+            .then(response => {
+                setRecipeData({
+                    recipeCarbs: response.nutrition.nutrients[1].amount,
+                    recipeProtein: response.nutrition.nutrients[7].amount,
+                    recipeFat: response.nutrition.nutrients[3].amount,
+                    recipeCalorie: response.nutrition.nutrients[2].amount,
                 }
                 )
             })
@@ -59,15 +205,18 @@ export default function RecipePage() {
                 {/* <h2 class=" text-3xl"
                     id = "recipePageHeader"
                     > {recipeData.title}</h2> */}
+                
+                
                 <img
-                class="rounded-lg"
+                class="recipeImage rounded-lg"
                 src={recipeData.img}
                 />    
+                
 
 
                 {/* <p id="stars">{result}</p> */}
                 
-                <div class="iconsdiv  flex items-center ">
+                <div class="iconsdiv flex items-center ">
                     <svg aria-hidden="true" class="w-10 h-10 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                     <svg aria-hidden="true" class="w-10 h-10 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                     <svg aria-hidden="true" class="w-10 h-10 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
@@ -101,8 +250,11 @@ export default function RecipePage() {
              {/*Add Review Button ( Routed to Reviews) & Add To Tracker */}
             
             <div className="buttonsContainer ml-20 justify-self-center">
-                <button type="button" class="AddReviewButton text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Add Review</button>
-                <button type="button" class="AddTrackerButton text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Add To Tracker</button>
+                <button type="button" onClick={addHandler} class="AddReviewButton text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                    Add Review</button>
+
+                <button type="button" class="AddTrackerButton text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                    Add To Tracker</button>
             </div>
             
             
@@ -111,7 +263,7 @@ export default function RecipePage() {
 
             {/*Ingredients Portion */}
             <div>
-                <h2 class="text-center text-3xl mb-2"
+                <h2 class="text-center text-3xl mb-2 mt-20"
                     id = "recipePageHeader" >Ingredients</h2>
 
                 <hr className=" " />
