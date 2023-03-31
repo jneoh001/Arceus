ab35c2d838f401cf4fdb7cbb9dfdd2e4c89f9af2
ShaoWei <laushaowei@gmail.com>
2023-03-31 15:37:14 +0800

    refined

--- arceus-app/src/components/RecipeCard/RecommendedRecipesList.jsx
+++ arceus-app/src/components/RecipeCard/RecommendedRecipesList.jsx
@@ -8,7 +8,6 @@ import RecipeCardHmepage from "./RecipeCardHmepage";
 
 const RecommendedRecipesList = () => {
   const { userDetails } = useAuth();
-  console.log(userDetails);
   const apiKey = "cb1c464d94f142c08b156c5beddade8b";
 
   const [id, setID] = useState();
@@ -19,24 +18,28 @@ const RecommendedRecipesList = () => {
       axios
         .get(
           `https://api.spoonacular.com/recipes/findByNutrients?apiKey=${apiKey}&minCarbs=${
-            userDetails.carbGoal / 3}
+            userDetails.carbGoal / 3
+          }
       &minCalories=${userDetails.calorieGoal / 3}&minProtein=${
             userDetails.proteinGoal / 3
           }&maxFat=${userDetails.fatGoal / 3}&number=5&random=true`
         )
         .then((res) => {
-          {/*response is an array, each recipe is an object inside. so i put inside recipeData which 
-        i initialize as an array*/}
-          console.log(res.data);
+          {
+            /*response is an array, each recipe is an object inside. so i put inside recipeData which 
+        i initialize as an array*/
+          }
+          // console.log(res.data);
           setRecipeData(res.data);
         })
         .catch((error) => console.log(error));
     }
   }, [userDetails]);
-  {/*then in return, i use map to go through every object in the array and get their data*/}
-  return (
-    (recipeData.map((recipe)=>(
-      <RecipeCardHmepage
+  {
+    /*then in return, i use map to go through every object in the array and get their data*/
+  }
+  return recipeData.map((recipe) => (
+    <RecipeCardHmepage
       id={recipe.id}
       name={recipe.title}
       carbs={recipe.carbs}
@@ -45,8 +48,7 @@ const RecommendedRecipesList = () => {
       calories={recipe.calories}
       img={recipe.image}
     />
-    )))
-  );
+  ));
 };
 
 export default RecommendedRecipesList;
