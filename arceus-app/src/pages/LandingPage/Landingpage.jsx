6d175e203c98e91c8b646d56a28ab678ab09ff44
Javier Neoh <jneoh001@e.ntu.edu.sg>
2023-04-03 01:12:47 +0800

    Restyled the Landing Page

--- arceus-app/src/pages/LandingPage/Landingpage.jsx
+++ arceus-app/src/pages/LandingPage/Landingpage.jsx
@@ -24,8 +24,32 @@ const LandingPage = () => {
                    style = {{backgroundColor: "#000000",
                              }}
     >
-      <div id="typewriter" className="auto">
-        <Typewriter />  
+      <div className="grid grid-rows-1 grid-cols-2">
+        <div id="typewriter" className="h-screen">
+          <Typewriter />  
+        </div>
+
+        <div className="h-screen text-white">
+         
+         {/* Options For Photo 
+         Book                    
+         https://images.unsplash.com/photo-1510172951991-856a654063f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bWluaW1hbCUyMGFlc3RoZXRpY3xlbnwwfHwwfHw%3D&w=1000&q=80
+         Plant 
+         https://i.imgur.com/5dmBrx6.jpg
+         Arceus 
+         https://i.pinimg.com/originals/b9/12/e7/b912e737b9d67659271be6afc4011efc.jpg */}
+          <div class="flex h-screen items-center justify-center bg-black pl-4 pr-6 mr-4">
+            <div class="max-w-sm overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl">
+              <img src=" https://i.imgur.com/5dmBrx6.jpg" alt="" class="w-full overflow-hidden" />
+              <div class="p-5">
+                <h2 className="font-bold text-gray-700 mb-2">About Us</h2>
+                <p class="text-medium mb-5 text-gray-700">We aim to provide you with a one-stop solution for your recipe needs!</p>
+                <button class="w-full rounded-md bg-gray-100 border border-black py-2 text-black hover:bg-yellow-500 hover:shadow-md duration-75">Get Started</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </ParallaxLayer>
     
